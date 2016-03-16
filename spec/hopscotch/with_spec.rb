f08116f7cc539c55f4199be818d53a781a9a9e5b
lachie <lachiec@gmail.com>
2016-03-17 08:28:33 +1100

    a few more specs

--- spec/hopscotch/with_spec.rb
+++ spec/hopscotch/with_spec.rb
@@ -19,6 +19,42 @@ describe Hopscotch::With do
     expect(result).to eq [:ok, "yeh"]
   end
 
+  it "doesn't match" do
+    result = Hopscotch::With.with do
+      match[:ok] = call(->(word) { [:err] })
+    end
+
+    expect(result[0]).to eq :err
+    expect(result[1]).to be_instance_of String
+  end
+
+  it "calls nomatch" do
+    result = Hopscotch::With.with do
+      match[:ok] = call(-> { [:err] })
+
+      nomatch do |_err, *things|
+        [:nomatch, *things]
+      end
+    end
+
+    expect(result).to eq [:nomatch]
+  end
+
+  it "calls final_match" do
+    input_word = "hey"
+
+    result = Hopscotch::With.with do
+      match[:ok, word ] = call(->(word) { [:ok, word] }, input_word)
+      match[:ok, word2] = call(->(word) { [:ok, word.reverse] }, word)
+
+      final_match do |_ok, word|
+        word.reverse
+      end
+    end
+
+    expect(result).to eq "hey"
+  end
+
   context Hopscotch::With::Definition do
     it "does the definition" do
       defn = Hopscotch::With::Definition.new do
